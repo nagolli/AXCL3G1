39124b368dd48fa113cde12e163dcddc662b4394
nagolli <nacho.gomis@gmail.com>
2017-10-09 13:21:29 +0200

    Creada Vista del Servidor
    Incializa la clase servidor con el parametro maximo en la sala, que se modifica en el main.
    Funciones para la vista:
    void SetClientes(int)
    void SetSalas(int)

---
+++ AXC G1L3 - Servidor/src/axc/g1l3/servidor/AXCG1L3Servidor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package axc.g1l3.servidor;
+
+/**
+ *
+ * @author Ignacio
+ */
+public class AXCG1L3Servidor
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Ventana ventanaInicial=new Ventana(50);
+        ventanaInicial.setVisible(true);
+    }
+    
+}
